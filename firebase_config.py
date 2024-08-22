7a752420f3a7c0015c265858f0e15664a587bea5
Kosuri Indu <kosurilindu@gmail.com>
2024-08-22 19:09:06 +0530

    Removed files

--- firebase_config.py
+++
@@ -1,6 +0,0 @@
-import firebase_admin
-from firebase_admin import credentials
-
-# Path to your service account key file
-cred = credentials.Certificate('"C:\Users\HP\OneDrive\Desktop\StockUp\stock-up-7292b-firebase-adminsdk-amvu2-671f84bd92.json"')
-firebase_admin.initialize_app(cred)
